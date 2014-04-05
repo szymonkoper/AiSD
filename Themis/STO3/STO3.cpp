5aad2bce4cdee7bde7aadad4074be680d9ccc387
Szymon Koper <sakydpozrux@gmail.com>
2014-04-05 15:25:56 +0200

    Progress in STO3

--- Themis/STO3/STO3.cpp
+++ Themis/STO3/STO3.cpp
@@ -1,6 +1,68 @@
 #include <cstdio>
+#include <vector>
+
+
+namespace Data
+{
+  short n; // # of rows
+  short m; // # of cols
+  
+  std::vector<std::vector<int> > t;
+  
+  void init_table()
+  {
+    t.resize(n);
+    for (std::vector<std::vector<int> >::iterator it = t.begin(); it != t.end(); ++it)
+    {
+      it->resize(m);
+    }
+  }
+  
+  void read_size()
+  {
+    short* rows = &n;
+    short* cols = &m;
+    scanf("%hd %hd", rows, cols);
+
+  }
+
+  void read_table()
+  {
+    int val;    
+
+    for (short x = 0; x < n; ++x)
+    {
+      for (short y = 0; y < m; ++y)
+      {
+        scanf("%d", &val);
+        t[x][y] = val;      
+      }
+    }
+  }
+  
+  namespace Debug
+  {
+    void print_table()
+    {
+      for (int x = 0; x < n; ++x)
+      {
+        for (int y = 0; y < m; ++y)
+        {
+          printf("%d ", t[x][y]);
+        }
+        printf("\n");
+      }
+    }
+  }
+}
+
 
 int main()
 {
-    return 0;
+  Data::read_size();
+  Data::init_table();
+  Data::read_table();
+ 
+  Data::Debug::print_table(); 
+  return 0;
 }
